c1353885fb46a8ece1fd0b3662b87ef5de122c79
Andrew Toth <andrewstoth@gmail.com>
2023-11-09 13:42:11 -0500

    Update docs for new types

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,8 @@
 //! - [`Vec`]
 //! - [`VecDeque`](std::collections::VecDeque)
 //! - [`LinkedList`](std::collections::LinkedList)
-//! - [`HashMap`](std::collections::HashMap) in the `Value` position
-//! - [`BTreeMap`](std::collections::BTreeMap) in the `Value` position
+//! - [`HashMap`](std::collections::HashMap) as the `Key` for all except `HeaderMap`, `Request`, and `Response`. As the `Value` for all types.
+//! - [`BTreeMap`](std::collections::BTreeMap)  as the `Key` only for `HeaderValue`, `StatusCode`, and `Version`. As the `Value` for all types.
 //! - [`HashSet`](std::collections::HashSet) for all except `HeaderMap`, `Request`, and `Response`
 //! - [`BTreeSet`](std::collections::BTreeSet) only for `HeaderValue`, `StatusCode`, and `Version`
 //!
@@ -57,8 +57,11 @@
 //!    #[serde(with = "http_serde_ext::header_map_generic::hash_map")]
 //!    hash_map: HashMap<String, HeaderMap<String>>,
 //!
-//!    #[serde(with = "http_serde_ext::status_code::btree_map")]
-//!    btree_map: BTreeMap<i32, StatusCode>,
+//!    #[serde(with = "http_serde_ext::status_code::btree_map_key")]
+//!    btree_map_key: BTreeMap<StatusCode, i32>,
+//!
+//!    #[serde(with = "http_serde_ext::authority::hash_set")]
+//!    hash_set: HashSet<uri::Authority>,
 //! }
 //! ```
 
@@ -76,13 +79,13 @@ enum Either<T> {
 }
 
 macro_rules! doc_mod {
-    ($ty:ty, $path:ident$(, $generic:ident)?$(; $extra:expr)?) => {
-        #[doc = concat!(" [`Serialize`](serde::Serialize)/[`Deserialize`](serde::Deserialize) for [`http::"$(, $extra)?, stringify!($ty), "`]")]
+    { $ty:ty, $path:ident$(, $generic:ident)? } => {
+        #[doc = concat!(" [`Serialize`](serde::Serialize)/[`Deserialize`](serde::Deserialize) for [`http::", stringify!($ty), "`]")]
         ///
         /// ```
         /// use std::{cmp::Ord, collections::*, hash::Hash};
         ///
-        #[doc = concat!("use http::", $($extra,)? stringify!($ty), ";")]
+        #[doc = concat!("use http::", stringify!($ty), ";")]
         /// use serde::{Serialize, Deserialize};
         ///
         /// #[derive(Serialize, Deserialize)]
@@ -117,19 +120,127 @@ macro_rules! doc_mod {
         /// }
         /// ```
         pub mod $path;
+    }
+}
+
+macro_rules! doc_mod_hash {
+    ($ty:ty, $path:ident$(, $extra:expr)?) => {
+        #[doc = concat!(" [`Serialize`](serde::Serialize)/[`Deserialize`](serde::Deserialize) for [`http::"$(, $extra)?, stringify!($ty), "`]")]
+        ///
+        /// ```
+        /// use std::{cmp::Ord, collections::*, hash::Hash};
+        ///
+        #[doc = concat!("use http::", $($extra,)? stringify!($ty), ";")]
+        /// use serde::{Serialize, Deserialize};
+        ///
+        /// #[derive(Serialize, Deserialize)]
+        /// struct MyStruct<T, U>
+        /// where
+        ///     U: Serialize + for<'a> Deserialize<'a>,
+        ///     T: Serialize + for<'a> Deserialize<'a> + Hash + Ord,
+        /// {
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "\")]")]
+        #[doc = concat!("    base: ", stringify!($ty), ",")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::option\")]")]
+        #[doc = concat!("    option: Option<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::result\")]")]
+        #[doc = concat!("    result: Result<", stringify!($ty), ", U>,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::vec\")]")]
+        #[doc = concat!("    vec: Vec<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::vec_deque\")]")]
+        #[doc = concat!("    vec_deque: VecDeque<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::linked_list\")]")]
+        #[doc = concat!("    linked_list: LinkedList<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::hash_map\")]")]
+        #[doc = concat!("    hash_map: HashMap<T, ", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::hash_map_key\")]")]
+        #[doc = concat!("    hash_map_key: HashMap<", stringify!($ty), ", U>,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::btree_map\")]")]
+        #[doc = concat!("    btree_map: BTreeMap<T, ", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::hash_set\")]")]
+        #[doc = concat!("    hash_set: HashSet<", stringify!($ty), ">,")]
+        /// }
+        /// ```
+        pub mod $path;
     };
 }
 
-doc_mod!(Authority, authority; "uri::");
+macro_rules! doc_mod_ord_and_hash {
+    ($ty:ty, $path:ident) => {
+        #[doc = concat!(" [`Serialize`](serde::Serialize)/[`Deserialize`](serde::Deserialize) for [`http::", stringify!($ty), "`]")]
+        ///
+        /// ```
+        /// use std::{cmp::Ord, collections::*, hash::Hash};
+        ///
+        #[doc = concat!("use http::", stringify!($ty), ";")]
+        /// use serde::{Serialize, Deserialize};
+        ///
+        /// #[derive(Serialize, Deserialize)]
+        /// struct MyStruct<T, U>
+        /// where
+        ///     U: Serialize + for<'a> Deserialize<'a>,
+        ///     T: Serialize + for<'a> Deserialize<'a> + Hash + Ord,
+        /// {
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "\")]")]
+        #[doc = concat!("    base: ", stringify!($ty), ",")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::option\")]")]
+        #[doc = concat!("    option: Option<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::result\")]")]
+        #[doc = concat!("    result: Result<", stringify!($ty), ", U>,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::vec\")]")]
+        #[doc = concat!("    vec: Vec<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::vec_deque\")]")]
+        #[doc = concat!("    vec_deque: VecDeque<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::linked_list\")]")]
+        #[doc = concat!("    linked_list: LinkedList<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::hash_map\")]")]
+        #[doc = concat!("    hash_map: HashMap<T, ", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::hash_map_key\")]")]
+        #[doc = concat!("    hash_map_key: HashMap<", stringify!($ty), ", U>,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::btree_map\")]")]
+        #[doc = concat!("    btree_map: BTreeMap<T, ", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::btree_map_key\")]")]
+        #[doc = concat!("    btree_map_key: BTreeMap<", stringify!($ty), ", U>,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::hash_set\")]")]
+        #[doc = concat!("    hash_set: HashSet<", stringify!($ty), ">,")]
+        ///
+        #[doc = concat!("    #[serde(with = \"http_serde_ext::", stringify!($path), "::btree_set\")]")]
+        #[doc = concat!("    btree_set: BTreeSet<", stringify!($ty), ">,")]
+        /// }
+        /// ```
+        pub mod $path;
+    };
+}
+
+doc_mod_hash!(Authority, authority, "uri::");
 doc_mod!(HeaderMap, header_map);
 doc_mod!(HeaderMap, header_map_generic, U);
-doc_mod!(HeaderName, header_name);
-doc_mod!(HeaderValue, header_value);
-doc_mod!(Method, method);
-doc_mod!(PathAndQuery, path_and_query; "uri::");
+doc_mod_hash!(HeaderName, header_name);
+doc_mod_ord_and_hash!(HeaderValue, header_value);
+doc_mod_hash!(Method, method);
+doc_mod_hash!(PathAndQuery, path_and_query, "uri::");
 doc_mod!(Request, request, U);
 doc_mod!(Response, response, U);
-doc_mod!(Scheme, scheme; "uri::");
-doc_mod!(StatusCode, status_code);
-doc_mod!(Uri, uri);
-doc_mod!(Version, version);
+doc_mod_hash!(Scheme, scheme, "uri::");
+doc_mod_ord_and_hash!(StatusCode, status_code);
+doc_mod_hash!(Uri, uri);
+doc_mod_ord_and_hash!(Version, version);
